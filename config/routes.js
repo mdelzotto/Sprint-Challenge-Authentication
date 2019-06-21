31aa82cb56ebd2c0cd20dfa5b6558a1b1ad7f0de
mdelzotto <madelzotto@comcast.net>
2019-06-21 12:01:34 -0400

    routes configured

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,7 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const db = require('../database/dbConfig');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -10,10 +13,68 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+        const user = req.body
+	      if (
+		            !user.username ||
+		            typeof user.username !== 'string' ||
+		            user.username === ''
+	      ) {
+		            res.status(400).json({ message: 'Username must be a valued string.' })
+	      } else if (
+		            !user.password ||
+		            typeof user.password !== 'string' ||
+		            user.password === ''
+	      ) {
+		            res.status(400).json({
+			                 message: 'Password must be a valued string.'
+		            })
+	      } else {
+		            const creds = req.body
+		            const hash = bcrypt.hashSync(creds.password, 14)
+		            creds.password = hash
+		            db('users')
+			                   .insert(creds)
+			                   .then(id => {
+				                          res.status(201).json(id)
+			                    })
+			                    .catch(() => {
+				                          res.status(500).json({ error: 'Unable to register user.' })
+			                    })
+	      }
+}
+
+function generateToken(user) {
+	       const payload = {
+		         username: user.username
+	       }
+
+	        const secret = process.env.JWT_SECRET
+
+	        const options = {
+		              expiresIn: '10m'
+	        }
+	        return jwt.sign(payload, secret, options)
 }
 
 function login(req, res) {
   // implement user login
+          const creds = req.body
+	        db('users')
+		              .where({ username: creds.username })
+		              .first()
+		              .then(user => {
+			                    if (user && bcrypt.compareSync(creds.password, user.password)) {
+				                            const token = generateToken(user)
+				                            res
+					                                 .status(200)
+					                                 .json({ message: `${user.username} is logged in`, token })
+			                    } else {
+				                          res.status(401).json({ message: 'You shall not pass!' })
+			                    }
+		               })
+		               .catch(() => {
+			                     res.status(500).json({ message: 'Please try logging in again.' })
+		               })
 }
 
 function getJokes(req, res) {
